b6080327c24cf7027a0b8338883455358139b83f
SHALU SHANAVAS <97325194+Shalushanavas@users.noreply.github.com>
2022-03-03 11:36:03 +0530

    Delete lambda.py

--- lambda.py
+++
@@ -1,6 +0,0 @@
-square = lambda s : s*s
-rectangle = lambda l,b : l*b
-triangle = lambda b,h : (b*h)/2
-print('Area of square',square(10))
-print('Area of triangle',triangle(7,10))
-print('Area of rectangle',rectangle(9,5))
